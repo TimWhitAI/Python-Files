1f6c84d121507aaecca0157b609c2561381c30e1
TimWhitAI <timwjobs072324@gmail.com>
2025-05-18 15:18:31 -0400

    Add files via upload
    Challenge: Ensure that the function works correctly with tuples of different lengths.
    =============================================
    Description: Create a function called      concat_tuples that takes two tuples as input and returns a new tuple containing all elements from both input tuples.

---
+++ TWAssignment17.py
@@ -0,0 +1,12 @@
+'''Challenge: Ensure that the function works correctly with tuples of different lengths.
+=============================================
+Description: Create a function called concat_tuples that takes two tuples as input and returns a new tuple containing all elements from both
+input tuples.'''
+
+def concat_tuples (tuple1, tuple2):
+        print (tuple1, tuple2)
+        return tuple1 + tuple2
+
+
+print (concat_tuples ([1, 2, 3, 4, 5, 9], [3, 4, 5, 6, 7]))
+
